a1c283a4139248e81e874f8dc036f8aedf3cdc4f
Víctor Terrón <quintanar@gmail.com>
2017-09-09 03:08:42 +0200

    ahora con decoradores de clases yeah!

--- 04.py
+++ 04.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 
 from base import *
 
-# En vez de copia y pega: decorador de métodos.
+# Aún mejor: decorador de clases.
 
 import time
 import logging
@@ -23,25 +23,33 @@ def log(func):
         return func(*args)
     return wrapped
 
+# Inspired from: https://stackoverflow.com/a/6307868
+def decorate_all_methods(decorator):
+    def wrapped(cls):
+        for attr_name in cls.__dict__:
+            attr = getattr(cls, attr_name)
+            if callable(attr):
+                # It's a function, decorate it
+                setattr(cls, attr_name, decorator(attr))
+        return cls
+    return wrapped
 
+@decorate_all_methods(log)
 class Cyborg(object):
 
-    @log
     def __init__(self, name):
         self.name = name
         self.weapon = DeathRay(ammunition=25)
         self.teleporter = TimeMachine()
 
-    @log
     def travel(self, destination, year):
         self.teleporter.go(destination, year)
         time.sleep(0.25)  # not instant, but almost
 
-    @log
     def attack(self, target):
         self.weapon.vaporize(target)
 
 robot = Cyborg('T-1000')
-robot.travel(destination='Los Angeles', year=1995)
+robot.travel('Los Angeles', 1995)
 robot.attack('Sarah Connor')
 robot.attack('John Connor')
